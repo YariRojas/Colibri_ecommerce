03ead4a0e50cc5ee5dd87be1340e00a0649993c3
YariRojas <137431787+YariRojas@users.noreply.github.com>
2023-07-24 21:31:39 -0600

    Se agrego a develop

---
+++ js/contactanos.js
@@ -0,0 +1,9 @@
+//Declaramos variables// mandamos llamar a los elementos
+let nombre = document.getElementById('nombre');
+let email = document.getElementById('email');
+let telefono = document.getElementById('telefono');
+let mensaje  = document.getElementById('mensaje');
+
+let btnSubmit = document.getElementsByClassName('btnSubmit');
+let esValidoEmail = true;
+
