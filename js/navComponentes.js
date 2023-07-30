4e23bb2c4773f89f3e0a7b5c0630e65e9ff7d78f
Aranza Quijano <aran_johnson@hotmail.com>
2023-07-30 10:53:02 -0600

    Se modifico navbar de navcomponets, se arreglaron estilos, se agrego imagen texto colibri a nosotros html

--- js/navComponentes.js
+++ js/navComponentes.js
@@ -3,62 +3,37 @@ let footer=document.getElementById("footer1");
 
 
 //Vamos a insertar nuestro navbar
+//PREGUNTAR A AMI COMO SOLUCIONAR EL HOVER Y ESPACIADO ENTRE LOS ICONS
 
 navbar.insertAdjacentHTML("afterbegin",`
 
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;600&display=swap" rel="stylesheet">
-<nav class=" " class="navbar navbar-expand-lg">
-<div class="container-fluid">
-    <img class="colibri" src="./img/colibri.png" alt="Colibrí" class="colibri" width="70px">  
-    </br> 
-                                                   
-  <a class="navbar-brand" href="#"></a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-    
-        <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="index.html">INICIO</a>
-     
-    </li>
-      <li class="nav-item">
-        <a class="nav-link" href="nosotros.html">NOSOTROS</a>
-      </li>
-      <li class="nav-item">
-      <a class="nav-link" href="contactanos.html">CONTÁCTANOS</a>
+<header class="container">
 
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="productos.html">PRODUCTOS</a>
-      
-    </li>
-  
-
-    <li class="nav-item">
-      <a class="nav-link" href="Carrito.html"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5Zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0ZM14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
-    </svg>
-   
-    </li>
-    <li>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
-      </svg>
-    </li>
-    <li>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search-heart" viewBox="0 0 16 16">
-        <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"/>
-       <path d="M13 6.5a6.471 6.471 0 0 1-1.258 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5ZM6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Z"/>
-    </svg>
-  </li>
-
-    </ul>
-  </div>
-</div>
-</nav>`);
+        <nav>
+                <img src="./img/colibri.png">
+              
+                <ul class="linksnav">
+                  <li><a href="./index.html">
+                      INICIO
+                    </a></li>
+                  <li><a href="./productos.html">PRODUCTOS</a></li>
+                  <li><a href="./nosotros.html">NOSOTROS</a></li>
+                  <li><a href="./contactanos.html">CONTÁCTANOS</a></li>              
+                </ul>  
+            
+        </nav>
+        <li class="icons"><a href="login.html"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+          <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/></svg></li>
+      <li class="icons"><a href="search"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-search-heart" viewBox="0 0 16 16">
+          <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018Z"/>
+          <path d="M13 6.5a6.471 6.471 0 0 1-1.258 3.844c.04.03.078.062.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1.007 1.007 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5ZM6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Z"/>
+        </svg></a></li>
+      <li class="icons"><a href="Carrito.html"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg></a></li>
+    </header>`);
 /*
 
       <li class="nav-item dropdown">
@@ -91,9 +66,11 @@ footer.insertAdjacentHTML("beforeend",`
           
           <ul class="nav flex-column">
             <li class="nav-item mb-3"><a href="./index.html" class="nav-link p-0 text-body-secondary">Inicio</a></li>
+            <li class="nav-item mb-2"><a href="./productos.html" class="nav-link p-0 text-body-secondary">Productos</a></li>
             <li class="nav-item mb-2"><a href="./nosotros.html" class="nav-link p-0 text-body-secondary">Nosotros</a></li>
             <li class="nav-item mb-2"><a href="./contactanos.html" class="nav-link p-0 text-body-secondary">Contáctanos</a></li>
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Login</a></li>
+            
         
           </ul>
         </div>
