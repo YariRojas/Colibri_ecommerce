2302eeb7f8896f451b3200e455d049c1f491ef63
Patricia Ortiz Tellez <ot.patricia1@gmail.com>
2023-08-13 20:53:22 -0600

    Se agregan cambios a la página de contáctanos al html, css y ks

--- js/contacto.js
+++ js/contacto.js
@@ -1,108 +1,74 @@
-function validar(){
-    let nombre = document.getElementById("nombre").value;
-    let email = document.getElementById("email").value;
-    let tel = document.getElementById("tel").value;
-    let mensaje = document.getElementById("mensaje").value;
+function validar() {
+    let nombre = document.getElementById("nombre");
+    let email = document.getElementById("email");
+    let tel = document.getElementById("tel");
+    let mensaje = document.getElementById("mensaje");
 
-    //RegExp
-
-    let regexNombre =  /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/;
+    let regexNombre = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+([A-Za-zÑñÁáÉéÍíÓóÚú]{3,40}\s*)))*$/;
     let regexCorreo = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let regexTel =   /^[0-9]{10}$/;
-    let regexMensaje = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/; 
-   
-    //Variables boolean
-    let nombreBoolean= true;
-    let emailBoolean = true;
-    let telBoolean = true;
-    let mensajeBoolean = true;
+    let regexTel = /^(?!0000000001)[0-9]{10}$/;
+    let regexMensaje = /^[\s\S]{10,250}$/;
 
-    //Validar
-    if (!regexNombre.test(nombre)){
+
+    let nombreValid = regexNombre.test(nombre.value);
+    let emailValid = regexCorreo.test(email.value);
+    let telValid = regexTel.test(tel.value);
+    let mensajeValid = regexMensaje.test(mensaje.value);
+
+    let errorMessages = [];
+
+    if (nombre.value.trim() === "") {
+        errorMessages.push("El campo Nombre no puede estar vacío.");
+        nombre.style.borderColor = "red";
+    } else if (!nombreValid) {
+        errorMessages.push("El nombre tiene que tener de 3 a 40 caracteres y solo debe contener letras y espacios.");
+        nombre.style.borderColor = "red";
+    } else {
+        nombre.style.borderColor = "green";
+    }
+
+    if (email.value.trim() === "") {
+        errorMessages.push("El campo Correo Electrónico no puede estar vacío.");
+        email.style.borderColor = "red";
+    } else if (!emailValid) {
+        errorMessages.push("El correo electrónico solo debe contener letras, números, puntos y guiones.");
+        email.style.borderColor = "red";
+    } else {
+        email.style.borderColor = "green";
+    }
+
+    if (tel.value.trim() === "") {
+        errorMessages.push("El campo Teléfono no puede estar vacío.");
+        tel.style.borderColor = "red";
+    } else if (!telValid) {
+        errorMessages.push("El teléfono solo debe contener números y su máximo son 10 dígitos.");
+        tel.style.borderColor = "red";
+    } else {
+        tel.style.borderColor = "green";
+    }
+
+    if (mensaje.value.trim() === "") {
+        errorMessages.push("El campo Comentarios no puede estar vacío.");
+        mensaje.style.borderColor = "red";
+    } else if (!mensajeValid) {
+        errorMessages.push("El mensaje debe ser de 10 a 250 caracteres y solo debe contener números, letras, puede contener caracteres especiales.");
+        mensaje.style.borderColor = "red";
+    } else {
+        mensaje.style.borderColor = "green";
+    }
+
+    if (errorMessages.length > 0) {
         Swal.fire({
             icon: 'error',
-            title: 'Nombre inválido',
-            text: 'Inténtalo de nuevo',
-          });
+            title: 'Corrige los siguientes errores:',
+            html: errorMessages.join("<br>"),
+        });
         return false;
-    }
-    else if (!regexCorreo.test(email)){
+    } else {
         Swal.fire({
-            icon: 'error',
-            title: 'Email inválido',
-            text: 'Inténtalo de nuevo',
-          });
-        return false;
+            icon: 'success',
+            title: 'Tus datos serán enviados correctamente a Colibrí',
+        });
+        return true;
     }
-    else if (!regexTel.test(tel)){
-        Swal.fire({
-            icon: 'error',
-            title: 'Teléfono inválido',
-            text: 'Inténtalo de nuevo',
-          });
-        return false;
-    }
-    else if (!regexMensaje.test(mensaje)){
-        Swal.fire({
-            icon: 'error',
-            title: 'Mensaje inválido',
-            text: 'Inténtalo de nuevo',
-          });
-        return false;}
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function validar(){
-    let nombre = document.getElementById("nombre").value;
-    let email = document.getElementById("email").value;
-let tel = document.getElementById("tel").value;
-    let mensaje = document.getElementById("mensaje").value;
-
-    //RegExp
-
-    let regexNombre =  /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/;
-    let regexCorreo = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let regexTel =   /^[0-9]{10}$/;
-    let regexMensaje = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/; 
-   
-    //Variables boolean
-    let nombreBoolean= true;
-    let emailBoolean = true;
-    let telBoolean = true;
-    let mensajeBoolean = true;
-
-    //Validar
-    if (!regexNombre.test(nombre)){
-        alert("nombre esta vacio");
-        return false;
-    }
-    else if (!regexCorreo.test(email)){
-        alert("Email esta vacio");
-        return false;
-    }
-    else if (!regexTel.test(tel)){
-        alert("Tel no es valido");
-        return false;
-    }
-    else if (!regexMensaje.test(mensaje)){
-        alert("Mensaje invalido");
-        return false;}
-    }
- */
+}
