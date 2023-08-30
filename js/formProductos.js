4568e3bbf4c578a65409cca10ef9d46e9e11cb73
Yaritza Rojas <yari.rr26@gmail.com~>
2023-08-29 23:36:04 -0600

    Se agrego fomulario de agregar nuevos producto, ya funciona correctamente

--- js/formProductos.js
+++ js/formProductos.js
@@ -1,37 +1,22 @@
-//elementos del html
 let txtNombreProducto = document.getElementById("txtNombreProducto");
 let txtDescriptionProducto = document.getElementById("txtDescriptionProducto");
-let txtPrecioProducto = document.getElementById("txtPrecioProducto"); // se declara variables
+let txtPrecioProducto = document.getElementById("txtPrecioProducto"); 
 let btnAgregar = document.getElementById("btnAgregar");
 let btnImg = document.getElementById("btnImg");
 let product_img = document.getElementById("product_img");
 let index = [];
-
 let productos = [];
-
-
-//parrafos de las alertas
 let alertNombre = document.getElementById("alertNombre");
 let alertDescrip = document.getElementById("alertDescrip");
 let alertPrecio = document.getElementById("alertPrecio");
 let alertImg = document.getElementById("alertImg");
-
-//div para alertas
 let alertValidaNombre = document.getElementById("alertValidaNombre");
 let alertValidaDescription = document.getElementById("alertValidaDescription");
 let alertValidaPrecioProducto = document.getElementById("alertValidaPrecioProducto");
 let alertValidaImg = document.getElementById("alertValidaImg");
 
-//se declaran funciones para las validaciones
 function validarNombre(nombre) {
-    if (nombre.length >= 3 && nombre.length < 50) {
-        return true;
-    } else {
-        return false;
-    }
-}
-function validarDescription(description) {
-    if (description.length >= 3 && description.length < 201) {
+    if (nombre.length >= 4 && nombre.length < 30) {
         return true;
     } else {
         return false;
@@ -44,78 +29,78 @@ function validarPrecio(precio) {
         return false;
     }
 }
+function validarDescription(description) {
+    if (description.length >= 5 && description.length < 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 btnAgregar.addEventListener("click", function (event) {
     event.preventDefault();
     if (!validarNombre(txtNombreProducto.value.trim())) {
         if (!index.includes("nombre")) {
             alertValidaNombre.insertAdjacentHTML(
-                "afterbegin", ` El <strong> Nombre </strong> no es correcto. <br/> `);
+                "afterbegin", `El nombre es incorrecto`);
             alertValidaNombre.style.color = "red";
             txtNombreProducto.style.border = "solid thin red";
             index.push("nombre");
         }
     }
-    if (!validarDescription(txtDescriptionProducto.value.trim())) {
-        if (!index.includes("description")) {
-            alertValidaDescription.insertAdjacentHTML(
-                "afterbegin", ` La <strong> Descripción </strong> no es correcta. <br/> `);
-            alertValidaDescription.style.color = "red";
-            txtDescriptionProducto.style.border = "solid thin red";
-            index.push("description");
-        }
-    }
     if (!validarPrecio(txtPrecioProducto.value.trim())) {
         if (!index.includes("precio")) {
             alertValidaPrecioProducto.insertAdjacentHTML(
-                "afterbegin", ` El <strong> Precio </strong> no es correcto. <br/> `);
+                "afterbegin", `El precio es incorrecto`);
             alertValidaPrecioProducto.style.color = "red";
             txtPrecioProducto.style.border = "solid thin red";
             index.push("precio");
         }
     }
-
+    if (!validarDescription(txtDescriptionProducto.value.trim())) {
+        if (!index.includes("description")) {
+            alertValidaDescription.insertAdjacentHTML(
+                "afterbegin", `La descripción es incorrecta`);
+            alertValidaDescription.style.color = "red";
+            txtDescriptionProducto.style.border = "solid thin red";
+            index.push("description");
+        }
+    }
     if (!product_img.src) {
         if (!index.includes("imagen")) {
             alertValidaImg.insertAdjacentHTML(
-                "afterbegin", ` Debe seleccionar una <strong>imagen</strong>.<br/> `);
+                "afterbegin", `Selecciona una imagen`);
             alertValidaImg.style.color = "red";
-            alertImg.style.border = "solid thin red";
             index.push("imagen");
         }
     }
     if (!product_img.src.match(/[^\s]+(.*?).(jpg|jpeg|png|JPG|JPEG|PNG)$/)) {
         if (!index.includes("imagen")) {
             alertValidaImg.insertAdjacentHTML(
-                "afterbegin", ` El formato de la <strong>imagen</strong> es incorrecto.<br/> `);
+                "afterbegin", `El formato de la imagen no es correcto`);
             alertValidaImg.style.color = "red";
-            alertImg.style.border = "solid thin red";
             index.push("imagen");
         }
     }
-
-
     if (!index.includes("nombre") && !index.includes("description") && !index.includes("price") && !index.includes("imagen")) {
         btnAgregar.disabled = true;
-        btnAgregar.textContent = "Agregando...";
+        btnAgregar.textContent = "Agregando nuevo producto...";
         btnAgregar.style.fontWeight = "bold";
         guardarProducto(txtNombreProducto.value, product_img.src, txtDescriptionProducto.value, txtPrecioProducto.value);
         Toast.fire({
             icon: 'success',
-            title: '¡El producto se registró con éxito!'
+            title: '¡EL NUEVO PRODUCTO HA SIDO REGISTRADO!'
         });
         limpiarTodo();
     }
 });
 
-function guardarProducto(name, src, description, price) {
+function guardarProducto(name, price, description, src) {
     let producto = `{
         "name": "${name}",
-        "img": "${src}",
+        "price": "${price}",
         "description": "${description}",
-        "price": "${price}"
-    }`;
-
+        "img": "${src}"}`;
     productos.push(JSON.parse(producto));
     this.localStorage.setItem("producto", JSON.stringify(productos));
 }
@@ -124,10 +109,9 @@ const Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
-    timer: 5000,
+    timer: 3000,
     timerProgressBar: true
 });
-
 
 window.addEventListener("load", function (event) {
     event.preventDefault();
@@ -135,17 +119,12 @@ window.addEventListener("load", function (event) {
     if (this.localStorage.getItem("producto") != null) {
         JSON.parse(this.localStorage.getItem("producto")).forEach((p) => {
             productos.push(p);
-        }//foreach
-        );
-
-    }//if
-
-}); // window // load
-
+        });
+    }
+});
 
 btnImg.addEventListener("click", function (event) {
     event.preventDefault();
-    // Cloudinary ================================================
     var myWidget = cloudinary.createUploadWidget({
         cloudName: 'dinq39jcf',
         uploadPreset: 'q7vp9g0r',
@@ -157,52 +136,21 @@ btnImg.addEventListener("click", function (event) {
         }
     });
     myWidget.open();
-
 }, false);
 
 
-//Listener para validar el nombre cada vez que el usuario teclee algo en el campo nombre
-txtNombreProducto.addEventListener("keyup", function (event) {
-    event.preventDefault();
+//validar campos
+txtNombreProducto.addEventListener("input", function () {
     if (!validarNombre(txtNombreProducto.value.trim())) {
-        if (!index.includes("nombre")) {
-            alertValidaNombre.insertAdjacentHTML(
-                "afterbegin", ` El <strong> Nombre </strong> no es correcto. <br/> `);
-            alertValidaNombre.style.color = "red";
-            txtNombreProducto.style.border = "solid thin red";
-            index.push("nombre");
-        }
-    }//if nombre del producto no cumple las validaciones
-    else {
-        //quitar alertas
+        alertValidaNombre.innerHTML = `El nombre es incorrecto`;
+        alertValidaNombre.style.color = "red";
+        txtNombreProducto.style.border = "solid thin red";
+        index.push("nombre");
+    } else {
         alertValidaNombre.innerHTML = "";
-        alertNombre.style.display = "none";
         txtNombreProducto.style.border = "";
         removeAllInstances(index, "nombre");
     }
-
-});
-
-
-txtDescriptionProducto.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (!validarDescription(txtDescriptionProducto.value.trim())) {
-        if (!index.includes("description")) {
-            alertValidaDescription.insertAdjacentHTML(
-                "afterbegin", ` La <strong> Descripción </strong> no es correcta. <br/> `);
-            alertValidaDescription.style.color = "red";
-            txtDescriptionProducto.style.border = "solid thin red";
-            index.push("description");
-        }
-    }//if description no cumple las validaciones
-    else {
-        //quitar alertas
-        alertValidaDescription.innerHTML = "";
-        alertDescrip.style.display = "none";
-        txtDescriptionProducto.style.border = "";
-        removeAllInstances(index, "description");
-    }
-
 });
 
 txtPrecioProducto.addEventListener("keyup", function (event) {
@@ -210,20 +158,18 @@ txtPrecioProducto.addEventListener("keyup", function (event) {
     if (!validarPrecio(txtPrecioProducto.value.trim())) {
         if (!index.includes("precio")) {
             alertValidaPrecioProducto.insertAdjacentHTML(
-                "afterbegin", ` El <strong> Precio </strong> no es correcto. <br/> `);
+                "afterbegin", `El precio es incorrecto`);
             alertValidaPrecioProducto.style.color = "red";
             txtPrecioProducto.style.border = "solid thin red";
             index.push("precio");
         }
-    }//if precio producto no cumple las validaciones 
+    }
     else {
-        //quitar alertas
         alertValidaPrecioProducto.innerHTML = "";
         alertPrecio.style.display = "none";
         txtPrecioProducto.style.border = "";
         removeAllInstances(index, "precio");
     }
-
 });
 
 txtPrecioProducto.addEventListener("change", function (event) {
@@ -231,36 +177,50 @@ txtPrecioProducto.addEventListener("change", function (event) {
     if (!validarPrecio(txtPrecioProducto.value.trim())) {
         if (!index.includes("precio")) {
             alertValidaPrecioProducto.insertAdjacentHTML(
-                "afterbegin", ` El <strong> Precio </strong> no es correcto. <br/> `);
+                "afterbegin", `El precio es incorrecto`);
             alertValidaPrecioProducto.style.color = "red";
             txtPrecioProducto.style.border = "solid thin red";
             index.push("precio");
         }
-    }//if precio producto no cumple las validaciones 
+    }
     else {
-        //quitar alertas
         alertValidaPrecioProducto.innerHTML = "";
         alertPrecio.style.display = "none";
         txtPrecioProducto.style.border = "";
         removeAllInstances(index, "precio");
     }
-
 });
 
+txtDescriptionProducto.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (!validarDescription(txtDescriptionProducto.value.trim())) {
+        if (!index.includes("description")) {
+            alertValidaDescription.insertAdjacentHTML(
+                "afterbegin", `La descripción es incorrecta`);
+            alertValidaDescription.style.color = "red";
+            txtDescriptionProducto.style.border = "solid thin red";
+            index.push("description");
+        }
+    }
+    else {
+        alertValidaDescription.innerHTML = "";
+        alertDescrip.style.display = "none";
+        txtDescriptionProducto.style.border = "";
+        removeAllInstances(index, "description");
+    }
+});
 
 product_img.addEventListener("load", function (event) {
     event.preventDefault();
     if (!product_img.src) {
         if (!index.includes("imagen")) {
             alertValidaImg.insertAdjacentHTML(
-                "afterbegin", ` Debe seleccionar una <strong>imagen</strong>.<br/> `);
+                "afterbegin", `Seleccione una imagen`);
             alertValidaImg.style.color = "red";
-            alertImg.style.border = "solid thin red";
             index.push("imagen");
         }
-    } //if la imagen no se selecciona 
+    }
     else {
-        //quitar alertas
         alertValidaImg.innerHTML = "";
         alertImg.style.display = "none";
         product_img.style.border = "";
@@ -269,14 +229,12 @@ product_img.addEventListener("load", function (event) {
     if (!product_img.src.match(/[^\s]+(.*?).(jpg|jpeg|png|JPG|JPEG|PNG)$/)) {
         if (!index.includes("imagen")) {
             alertValidaImg.insertAdjacentHTML(
-                "afterbegin", ` El formato de la <strong>imagen</strong> es incorrecto.<br/> `);
+                "afterbegin", ` El formato de la imagen no es correcto`);
             alertValidaImg.style.color = "red";
-            alertImg.style.border = "solid thin red";
             index.push("imagen");
         }
-    }//else if la imagen (formato) no cumple las validaciones  
+    }
     else {
-        //quitar alertas
         alertValidaImg.innerHTML = "";
         alertImg.style.display = "none";
         product_img.style.border = "";
@@ -284,13 +242,12 @@ product_img.addEventListener("load", function (event) {
     }
 });
 
-//Remueve todas las instancias de un objeto dado (item) que se encuentre en el arreglo index
+//limpiar
 function removeAllInstances(arr, item) {
     for (var i = arr.length; i--;) {
         if (arr[i] === item) arr.splice(i, 1);
     }
 }
-
 function limpiarTodo() {
     index = [];
     txtNombreProducto.value = "";
@@ -303,6 +260,6 @@ function limpiarTodo() {
     product_img.src = "";
     btnAgregar.disabled = false;
     btnAgregar.textContent = "Agregar";
-    btnAgregar.style.fontWeight = "bold";
+    btnAgregar.style.fontWeight = "";
 }
 
